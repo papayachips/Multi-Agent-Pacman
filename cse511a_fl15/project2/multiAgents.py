f4bbfe0f141be06574227990acb7f27c08aa16e2
chengming@ACCOUNTS.AD.WUSTL.EDU <chengming@ACCOUNTS.AD.WUSTL.EDU@cc2aa20a-c44b-420d-92d6-103f9eeb1e34>
2015-10-08 18:27:01 +0000

    question5

--- cse511a_fl15/project2/multiAgents.py
+++ cse511a_fl15/project2/multiAgents.py
@@ -399,10 +399,15 @@ def betterEvaluationFunction(currentGameState):
   
   ghostPositions =  successorGameState.getGhostPositions()
   distance = [math.sqrt(math.pow(newPos[0] - ghostPosition[0],2) + math.pow(newPos[1] - ghostPosition[1], 2)) for ghostPosition in ghostPositions]
-  indexes = [newScaredTimes.index(scared_time) for scared_time in newScaredTimes if scared_time > 0]
+  indexes = [newScaredTimes.index(scared_time) for scared_time in newScaredTimes if scared_time > 2]
   if (indexes == [] and not all(dist >= 1.5 for dist in distance)):
     score -= 70
-
+  dist_indexes = [distance.index(dist) for dist in distance if dist < 1.5]
+  if (indexes is not [] and indexes == dist_indexes):
+    score += 0
+  if (indexes is not [] and indexes is not dist_indexes):
+    score -70
+    #indexes_modified = [index in indexes if (distance[index] < 1.5 and newScaredTimes[index] > distance[index])]
 
 
   return successorGameState.getScore() + score
