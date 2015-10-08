d247a20936294ba4a9c6370b059adb95ca1c186f
chengming@ACCOUNTS.AD.WUSTL.EDU <chengming@ACCOUNTS.AD.WUSTL.EDU@cc2aa20a-c44b-420d-92d6-103f9eeb1e34>
2015-10-08 18:34:36 +0000

    question5

--- cse511a_fl15/project2/multiAgents.py
+++ cse511a_fl15/project2/multiAgents.py
@@ -404,7 +404,7 @@ def betterEvaluationFunction(currentGameState):
     score -= 70
   dist_indexes = [distance.index(dist) for dist in distance if dist < 1.5]
   if (indexes is not [] and indexes == dist_indexes):
-    score += 0
+    score += 100
   if (indexes is not [] and indexes is not dist_indexes):
     score -70
     #indexes_modified = [index in indexes if (distance[index] < 1.5 and newScaredTimes[index] > distance[index])]
